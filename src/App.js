8ac2ce94e44d68b9b8d0cfccdd44f9c26cb747a4
kyBa-Le <lekyba2000hc@gmail.com>
2025-04-22 09:29:48 +0700

    revers to origin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { Item } from './components/Item';
-import { ProductLists } from './task/Task2';
+
 
 function App() {
   return (
     <>
-    <ProductLists></ProductLists>
     </>
   );
 }
